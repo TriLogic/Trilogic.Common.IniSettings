fc7458711086127d9077aff9a9909b0ae96d714b
Andrew Friedl <affriedl@hotmail.com>
2018-08-21 22:44:59 -0500

    Add project files.

---
+++ TriLogic.Common.IniSettings/IniException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trilogic.Common.Settings
+{
+    public class IniException : Exception
+    {
+        public IniException(string msg)
+            : base(msg)
+        {
+        }
+    }
+}
---
+++ TriLogic.Common.IniSettings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Trilogic.Common.Settings")]
+[assembly: AssemblyDescription("Simplify access to Arg and INI Settings")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trilogic Industries, LLC")]
+[assembly: AssemblyProduct("Trilogic.Common.Settings")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e25800d-8dea-4f14-b399-f2c2cd63ca51")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
